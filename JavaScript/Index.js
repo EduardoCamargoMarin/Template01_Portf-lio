356226f2d60732c214462c95243c6a0a52bf9e83
Eduardo Marin <du.marin10@gmail.com>
2023-01-20 21:24:34 -0300

    Template_01

---
+++ JavaScript/Index.js
@@ -0,0 +1,78 @@
+//Mostrar lista de serviços
+
+
+//servico 01
+let list01 = document.getElementById('lists1')
+
+function showServico01() {
+    if (list01 ===1) {
+        document.getElementById('servico01').innerHTML = ''
+        document.getElementById('servico01').style.borderBottom = 'none'
+        return list01 = 0
+        
+    }
+    else {
+        document.getElementById('servico01').innerHTML = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi corporis tenetur at qui aut maiores quod id itaque doloribus obcaecati a aliquam officiis, assumenda, voluptas et dolore numquam cupiditate quibusdam.'
+        document.getElementById('servico01').style.lineHeight = '30px'
+        document.getElementById('servico01').style.borderBottom = '2px solid black'
+
+        return list01 = 1
+    }
+}
+
+//servico 02
+let list02 = document.getElementById('lists2')
+
+function showServico02() {
+    if (list02 ===1) {
+        document.getElementById('servico02').innerHTML = ''
+        document.getElementById('servico02').style.borderBottom = 'none'
+        return list02 = 0
+        
+    }
+    else {
+        document.getElementById('servico02').innerHTML = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi corporis tenetur at qui aut maiores quod id itaque doloribus obcaecati a aliquam officiis, assumenda, voluptas et dolore numquam cupiditate quibusdam.'
+        document.getElementById('servico02').style.lineHeight = '30px'
+        document.getElementById('servico02').style.borderBottom = '2px solid black'
+
+        return list02 = 1
+    }
+}
+
+//servico 03
+let list03 = document.getElementById('lists3')
+
+function showServico03() {
+    if (list03 ===1) {
+        document.getElementById('servico03').innerHTML = ''
+        document.getElementById('servico03').style.borderBottom = 'none'
+        return list03 = 0
+        
+    }
+    else {
+        document.getElementById('servico03').innerHTML = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi corporis tenetur at qui aut maiores quod id itaque doloribus obcaecati a aliquam officiis, assumenda, voluptas et dolore numquam cupiditate quibusdam.'
+        document.getElementById('servico03').style.lineHeight = '30px'
+        document.getElementById('servico03').style.borderBottom = '2px solid black'
+
+        return list03 = 1
+    }
+}
+
+//servico 04
+let list04 = document.getElementById('lists4')
+
+function showServico04() {
+    if (list04 ===1) {
+        document.getElementById('servico04').innerHTML = ''
+        document.getElementById('servico04').style.borderBottom = 'none'
+        return list04 = 0
+        
+    }
+    else {
+        document.getElementById('servico04').innerHTML = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi corporis tenetur at qui aut maiores quod id itaque doloribus obcaecati a aliquam officiis, assumenda, voluptas et dolore numquam cupiditate quibusdam.'
+        document.getElementById('servico04').style.lineHeight = '30px'
+        document.getElementById('servico04').style.borderBottom = '2px solid black'
+
+        return list04 = 1
+    }
+}
